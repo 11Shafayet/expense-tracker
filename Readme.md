0c8b11a81fd959e1dc193f6295fce16db2e88ad7
Shafayet <11shafayet@gmail.com>
2024-08-01 10:24:50 +0600

    updated

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# This is a simple expense tracker app
+
+You can manage your expense with this app. You can search, add category add so on. A complete expense solution.
+This site is built with MERN.
+Visit my portfolio for more porjects like this.
+https://shafayet.me/
--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  'mongodb+srv://Shafayet:Shafayet111@cluster0.8laudjn.mongodb.net/?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
